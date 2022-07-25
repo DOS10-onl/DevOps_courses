97d96ace90ac0760ce3d3719b9846abecf9f9ffb
Dmitry Yankov <dmitryyankou@gmail.com>
2022-07-25 15:30:00 +0300

    fix remarks

--- lesson13/lesson13_script1.sh
+++ lesson13/lesson13_script1.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-
+mkdir /home/$USER/lesson13
 # have two strings inside: Hello string and current date-time
-touch /home/dmitryyankov/lesson13/two.txt
+touch /home/$USER/lesson13/two.txt
 
-echo "Hello string" > /home/dmitryyankov/lesson13/two.txt
-echo `date` >> /home/dmitryyankov/lesson13/two.txt
+echo "Hello string" > /home/$USER/lesson13/two.txt
+echo `date` >> /home/$USER/lesson13/two.txt
 # empty file with 777 permissions set
-touch /home/dmitryyankov/lesson13/777.txt
-# echo "777" > /home/dmitryyankov/lesson13/777.txt
-chmod 777 /home/dmitryyankov/lesson13/777.txt
+touch /home/$USER/lesson13/777.txt
+# echo "777" > /home/$USER/lesson13/777.txt
+chmod 777 /home/$USER/lesson13/777.txt
 
 # have one random string
-echo $RANDOM | md5sum | head -c 20 > /home/dmitryyankov/lesson13/random.txt
+echo $RANDOM | md5sum | head -c 20 > /home/$USER/lesson13/random.txt
 
 # just empty file
-touch /home/dmitryyankov/lesson13/file{1..2}.txt
+touch /home/$USER/lesson13/file{1..2}.txt
