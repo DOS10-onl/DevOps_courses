889e6a8754418ee45e8e1e77c76989b5a0586f7e
Dmitry Yankov <dmitryyankou@gmail.com>
2022-08-08 18:50:15 +0300

    fix shellcheck

--- lesson12/lesson12.sh
+++ lesson12/lesson12.sh
@@ -6,6 +6,7 @@ touch ~/lesson12/lesson12.txt
 # Add three strings in the file using echo command
 echo "date" > ~/lesson12/lesson12.txt
 echo 'date' >> ~/lesson12/lesson12.txt
+# shellcheck disable=SC2046,SC2005,SC2006
 echo `date` >> ~/lesson12/lesson12.txt
 
 # Define three different variables
